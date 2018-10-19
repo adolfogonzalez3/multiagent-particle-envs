971b1f4c674c17c08f2f49a7295b45a4c03f96d2
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2018-10-19 15:08:42 -0500

    Made MailboxInSync. The class creates a central queue that other producers put their input into. Other producers are given MailboxSpawn that inputs into the central queue of the MailboxInSync that spawned it. The MailboxInSync also broadcasts to other producers.

--- multiagent/EnvironmentInSync.py
+++ multiagent/EnvironmentInSync.py
@@ -1,13 +1,21 @@
 
+from collections import namedtuple
+
 from gym import Env
 
 from multiagent.environment import MultiAgentEnv, BatchMultiAgentEnv
 
+from multiagent.MailboxInSync import MailboxInSync
+
+SpawnAction = namedtuple('SpawnAction', ['ID', 'action'])
+
 class EnvironmentSpawn(Env):
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, mailbox):
+        self._mailbox = mailbox
         
     def step(self, action):
+        self._mailbox.append(action)
+        return self._mailbox.get()
     
     def reset(self):
         pass
@@ -23,7 +31,12 @@ class EnvironmentInSync(MultiAgentEnv):
     def __init__(self, *args, **kwargs):
         self.instance_ids = []
         self.id_counter = 0
+        self.mailbox = MailboxInSync()
         super().__init__(*args, **kwargs)
         
     def spawn(self):
+        new_id = self.id_counter
+        self.id_counter += 1
+        return EnvironmentSpawn(mailbox.spawn())
         
+    
